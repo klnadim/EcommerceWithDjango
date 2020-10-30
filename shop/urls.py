59b744d55dacff9805f79590af036ec03244924f
Nadim <klnadim27@gmail.com>
2020-10-30 18:18:16 +0600

    Adding Cart PopOver & Other Pages Using Bootstrap | Python Django Tutorials In Hindi #40

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
    path('', views.index,name="ShopHome"),
-   path('about', views.about,name="About"),
+   path('about/', views.about,name="About"),
+   path('contact/', views.contact,name="Contact"),
+   path('tracker/', views.tracker,name="Tracker"),
+   path('search/', views.search,name="Search"),
+   path('productView/', views.productView,name="productView"),
+   path('checkout/', views.checkout,name="Checkout"),
    path('programtest',views.programTest,name="programTest")
 ]
--- shop/views.py
+++ shop/views.py
@@ -55,4 +55,17 @@ def programTest(request):
 def about(request):
     return render(request, 'shop/about.html')
 
+def contact(request):
+    return render(request, 'shop/contact.html')
 
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def search(request):
+    return render(request, 'shop/search.html')
+
+def productView(request):
+    return render(request, 'shop/productView.html')
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
